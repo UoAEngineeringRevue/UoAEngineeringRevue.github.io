1d53edb5cc26b4398a5d51107709dd948e56d3b6
Daniel <dan_wong@outlook.com>
2018-05-07 22:42:38 +1200

    New design

--- js/custom.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    $("#downarrow").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#about").offset().top
-        }, 1000);
-    });
-});
